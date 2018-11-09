56d31dfe6dc725549856511f8001ec52ad9c0e95
Jack Bogart <bogart.jack98@gmail.com>
2018-11-09 08:06:16 -0700

    stuff

--- clientside/src/Containers/TidContainer.js
+++ clientside/src/Containers/TidContainer.js
@@ -1,5 +1,6 @@
 import {React, Component} from 'react';
 import axios from 'axios';
+import TidList from './tids/TidList';
 class TidContainer extends Component {
 	constructor(props) {
 		super(props);
@@ -12,6 +13,13 @@ class TidContainer extends Component {
 		axios.get('/api/v1/data/huffingtonPolls').then(response =>{
 			this.setState({
 				tids: response.items
-		});
+		}).catch(err=>{console.log(err)});
+	}
+	render(){
+		return(
+			<div>
+				<TidList tids = {this.state.tids} />
+			</div>
+		)
 	}
 } 		
